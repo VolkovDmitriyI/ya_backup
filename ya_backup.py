d71141562ae073bfad7a7d7cfdb4bddf0542d7a7
VolkovDmitriyI <104028690+VolkovDmitriyI@users.noreply.github.com>
2023-07-01 18:50:03 +0400

    Add files via upload

---
+++ ya_backup.py
@@ -0,0 +1,70 @@
+import os.path
+import shutil
+import yadisk
+import telebot
+import sys
+import json
+from datetime import datetime
+
+
+class Settings:
+    __slots__ = (
+        "Telegram",
+        "BotApiKey",
+        "TelegramChat",
+        "YaDisk",
+        "YaDiskToken",
+        "BackupCatalog",
+    )
+
+    @staticmethod
+    def handle_error(exc):
+        print(exc)
+        exit(-1)
+
+    def __init__(self):
+        try:
+            settings_file = sys.argv[1]
+            user_settings = json.load(open(settings_file))
+
+            for user_settings_key in user_settings:
+                setattr(self, user_settings_key, user_settings[user_settings_key])
+
+        except IndexError as exc:
+            self.handle_error(f"Provide config name. Exception: {exc}")
+        except FileNotFoundError as exc:
+            self.handle_error(f"File not found. Exception: {exc}")
+        except json.JSONDecodeError as exc:
+            self.handle_error(f"Json error. Exception: {exc}")
+        except AttributeError as exc:
+            self.handle_error(f"No settings available. Exception: {exc}")
+
+
+disk = yadisk.YaDisk(token=Settings().YaDiskToken)
+bot = telebot.TeleBot(Settings().BotApiKey)
+chatId = Settings().TelegramChat
+
+
+def send_message(message):
+    bot.send_message(chatId, message)
+
+
+if not disk.check_token():
+    send_message("Токен яндекс диска не валиден")
+    exit(-1)
+
+
+def create_archive(path):
+    if os.path.exists(path):
+        dt = datetime.now()
+        archivename = path+'_'+dt.strftime('%d_%m_%Y')
+        shutil.make_archive(archivename, 'zip', path)
+        disk.upload(archivename+'.zip', "/backup/"+archivename)
+    else:
+        send_message(f"Путь {path} не существует")
+
+
+for BackupCatalog in Settings().BackupCatalog:
+    create_archive(BackupCatalog)
+
+
